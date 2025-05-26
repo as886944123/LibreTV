96253ba024bfefc2e71e7582a716e45afc03d505
as886944123 <58317138+as886944123@users.noreply.github.com>
2025-05-26 15:53:03 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,7 +110,8 @@ const API_SITES = {
         name: 'iKun资源'
     },
     sn: {
-    api: 'https://suoniapi.com/api.php/provide/vod/?ac=list',
+    api: 'https://suoniapi.com/api.php/provide/vod/',
+    detail: 'https://suoniapi.com', 
     name: '索尼资源'
     },
     testSource: {
