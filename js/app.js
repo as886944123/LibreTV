94da3ca039101a6ca7a411b2a0241ba84e374093
as886944123 <58317138+as886944123@users.noreply.github.com>
2025-07-29 19:15:30 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // 全局变量
-let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["maotai", "tyyszy","dyttzy", "bfzy", "ruyi","xiaomaomi", "ffzy","heimuer", "iqiyi", "wolong","hwba", "jisu","dbzy", "mozhua", "mdzy","zuid", "yinghua", "baidu","wujin", "wwzy","ikun", "lzi", "mdzy"]'); // 默认选中资源
+let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["maotai", "tyyszy","dyttzy", "bfzy", "ruyi","xiaomaomi", "ffzy","heimuer", "iqiyi", "wolong","hwba", "jisu","dbzy", "mozhua", "mdzy","zuid", "yinghua", "baidu","wujin", "wwzy","ikun", "lzi", "mdzy", "zy360"]'); // 默认选中资源
 let customAPIs = JSON.parse(localStorage.getItem('customAPIs') || '[]'); // 存储自定义API列表
 
 // 添加当前播放的集数索引
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
         // 默认选中资源
-        selectedAPIs = ["maotai", "tyyszy","dyttzy", "bfzy", "ruyi","xiaomaomi", "ffzy","heimuer", "iqiyi", "wolong","hwba", "jisu","dbzy", "mozhua", "mdzy","zuid", "yinghua", "baidu","wujin", "wwzy","ikun", "lzi", "mdzy"];
+        selectedAPIs = ["maotai", "tyyszy","dyttzy", "bfzy", "ruyi","xiaomaomi", "ffzy","heimuer", "iqiyi", "wolong","hwba", "jisu","dbzy", "mozhua", "mdzy","zuid", "yinghua", "baidu","wujin", "wwzy","ikun", "lzi", "mdzy", "zy360"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
 
         // 默认选中过滤开关
